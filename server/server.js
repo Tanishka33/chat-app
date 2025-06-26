176148d201dc7cb37ef294e623e84f833f651b96
Tanishka33 <tanishka.3315@gmail.com>
2025-06-26 15:29:06 +0530

    Connected the Routes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
+import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 // Create express app and HTTP server
 const app = express();
@@ -11,7 +13,12 @@ const server = http.createServer(app)
 app.use(express.json({limit:"4mb"}));
 app.use(cors());
 
+// Routes setup
 app.use("/api/status",(req,res)=>res.send("Server is live"));
+app.use("/api/auth", userRouter)
+
+// Connect to MongoDb
+await connectDB();
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, ()=>console.log("Server is running on PORT: "+ PORT));
