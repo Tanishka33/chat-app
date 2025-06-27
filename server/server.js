7805f07005184b117412dbdd78b815f2141dc18d
Tanishka33 <tanishka.3315@gmail.com>
2025-06-27 19:17:27 +0530

    Connecting frontend with backend

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
 import { Server } from "socket.io";
-import { use } from "react";
+// import { use } from "react";
 
 // Create express app and HTTP server
 const app = express();
